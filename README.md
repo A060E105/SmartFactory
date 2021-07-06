8f52f41c83b8a41b11e86491accdb1bfb3eeb35a
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2021-07-06 13:11:40 +0800

    feat: 新增錄音、存檔與取得輸入裝置的函式

---
+++ README.md
@@ -0,0 +1,12 @@
+# Audio
+
+## function
+
+record
+> 錄音
+
+save_wav
+> 儲存成wav檔
+
+getDevice
+> 取得輸入裝置
---
+++ audio.py
@@ -0,0 +1,38 @@
+import wave
+from pyaudio import PyAudio, paInt16
+import tqdm
+
+
+# recording
+def record():
+	pa = PyAudio()
+	stream = pa.open(format=paInt16,
+	 channels=1, 
+	 rate=48000, 
+	 input=True, 
+	 frames_per_buffer=2048)
+	my_buf = []
+	for _ in tqdm.trange(50):	# record  time
+		string_audio_data = stream.read(2048)
+		my_buf.append(string_audio_data)
+	stream.close()
+	return my_buf
+
+# show all input device
+def getDevice():
+	p = PyAudio()
+	info = p.get_host_api_info_by_index(0)
+	numdevice = info.get('deviceCount')
+	for i in range(0, numdevice):
+		print (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels'))
+		if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+			pass
+
+# save data to wav file
+def save_wav(filename, data):
+	wf = wave.open(f"{filename}.wav", "wb")
+	wf.setnchannels(1)
+	wf.setsampwidth(2)
+	wf.setframerate(48000)
+	wf.writeframes(b"".join(data))
+	wf.close()
